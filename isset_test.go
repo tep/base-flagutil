cadec35d94ef90079d684716a21e524969f3a4cc
Tim Peoples <tim@toolman.org>
2017-08-12 15:21:36 -0700

    Add unit tests for ValueIsSet()

---
+++ isset_test.go
@@ -0,0 +1,120 @@
+package flagutil
+
+import (
+	"flag"
+	"testing"
+
+	"github.com/spf13/pflag"
+)
+
+const (
+	boolFlagName  = "bool_flag"
+	testFlagName  = "string_flag"
+	testFlagValue = "the-value"
+)
+
+type flagPackage int
+
+const (
+	fpNone flagPackage = iota
+	fpBase
+	fpSPF13
+)
+
+type isSetTestcase struct {
+	name string
+	fpkg flagPackage
+	want string
+	args []string
+}
+
+func newIsSetTestcase(fpkg flagPackage, set bool) *isSetTestcase {
+	var name string
+
+	switch {
+	case fpkg == fpNone:
+		name = "no-flags-defined"
+
+	case fpkg == fpBase && set:
+		name = "base-flag-isset"
+
+	case fpkg == fpBase:
+		name = "base-flag-noset"
+
+	case fpkg == fpSPF13 && set:
+		name = "spf13-pflag-isset"
+
+	case fpkg == fpSPF13:
+		name = "spf13-pflag-noset"
+	}
+
+	tc := &isSetTestcase{
+		name: name,
+		fpkg: fpkg,
+	}
+
+	if fpkg == fpNone {
+		return tc
+	}
+
+	if set {
+		tc.want = testFlagValue
+		tc.args = []string{"--" + testFlagName, testFlagValue}
+	} else {
+		tc.args = []string{"--" + boolFlagName}
+	}
+
+	return tc
+}
+
+func (tc *isSetTestcase) test(t *testing.T) {
+	merged = false
+	rawArgs = []string{}
+
+	flag.CommandLine = flag.NewFlagSet("nodef:"+tc.name, flag.ContinueOnError)
+	pflag.CommandLine = pflag.NewFlagSet("nodef:"+tc.name, pflag.ContinueOnError)
+
+	var want string
+	var wset bool
+
+	if tc.fpkg != fpNone {
+		rawArgs = tc.args
+
+		want = tc.want
+		dval := "default-value"
+
+		if want == "" {
+			want = dval
+		} else {
+			wset = true
+		}
+
+		switch tc.fpkg {
+		case fpBase:
+			flag.String(testFlagName, dval, "A test flag")
+			flag.Bool(boolFlagName, false, "A bool flag")
+
+		case fpSPF13:
+			pflag.String(testFlagName, dval, "A test flag")
+			pflag.Bool(boolFlagName, false, "A bool flag")
+		}
+	}
+
+	if got, set, err := ValueIsSet(testFlagName); err != nil || got != want || set != wset {
+		t.Errorf("ValueIsSet(%q) == (%q, %v, %v); Wanted(%q, %v, %v)", testFlagName, got, set, err, want, wset, nil)
+	}
+}
+
+func TestValueIsSet(t *testing.T) {
+	tests := []*isSetTestcase{
+		newIsSetTestcase(fpNone, false),
+		newIsSetTestcase(fpBase, false),
+		newIsSetTestcase(fpSPF13, false),
+		newIsSetTestcase(fpBase, true),
+		newIsSetTestcase(fpSPF13, true),
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, tc.test)
+	}
+}
