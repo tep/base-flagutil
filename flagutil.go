bf30dacbd11d6e68305f446f9c714e3b63b05725
Tim Peoples <tim@toolman.org>
2017-08-12 09:52:07 -0700

    Add support for spf13/pflag (with merging)

--- flagutil.go
+++ flagutil.go
@@ -1,37 +1,94 @@
-package flagutil  // import "toolman.org/base/flagutil"
+package flagutil // import "toolman.org/base/flagutil"
 
 import (
+	"errors"
 	"flag"
+	"fmt"
+
+	"github.com/spf13/pflag"
 )
 
 // TODO(tep): Write Tests!!
 
-func ValueIsSet(name string) (string, bool) {
-	if !flag.Parsed() {
-		flag.Parse()
+var merged bool
+
+func Merged() bool {
+	return merged
+}
+
+func ValueIsSet(name string) (string, bool, error) {
+	if err := MergeAndParse(); err != nil {
+		return "", false, err
 	}
 
 	var isSet bool
-	value := flag.Lookup(name).Value.String()
+	value := pflag.Lookup(name).Value.String()
 
-  flag.Visit(func(f *flag.Flag){
-    if f.Name == name {
-      isSet = true
-			value = f.Value.String()
-    }
-  })
+	pflag.Visit(func(pf *pflag.Flag) {
+		if pf.Name == name {
+			isSet = true
+			value = pf.Value.String()
+		}
+	})
 
-	return value, isSet
+	return value, isSet, nil
 }
 
 func SetUnless(name, value string) (bool, error) {
-	if _, ok := ValueIsSet(name); ok {
+	if _, ok, err := ValueIsSet(name); err != nil {
+		return false, err
+	} else if ok {
 		return false, nil
 	}
 
-  if err := flag.Set(name, value); err != nil {
-    return false, err
-  }
+	if err := pflag.Set(name, value); err != nil {
+		return false, err
+	}
 
-  return true, nil
+	return true, nil
+}
+
+func MergeFlags() error {
+	if merged {
+		return nil
+	}
+
+	if flag.Parsed() {
+		return errors.New("cannot merge flags from package \"flag\": package already parsed")
+	}
+
+	if pflag.Parsed() {
+		return errors.New("cannot merge flags into package \"github.com/spf13/pflag\": package already parsed")
+	}
+
+	var cnf []string
+
+	flag.VisitAll(func(f *flag.Flag) {
+		if pf := pflag.CommandLine.Lookup(f.Name); pf != nil {
+			cnf = append(cnf, f.Name)
+			return
+		}
+		pflag.CommandLine.AddGoFlag(f)
+	})
+
+	if len(cnf) > 0 {
+		sfx := ""
+		if len(cnf) > 1 {
+			sfx = "s"
+		}
+		return fmt.Errorf("name conflict%s merging flags: %v", sfx, cnf)
+	}
+
+	merged = true
+	return nil
+}
+
+func MergeAndParse() error {
+	if err := MergeFlags(); err != nil {
+		return err
+	}
+
+	pflag.Parse()
+
+	return nil
 }
